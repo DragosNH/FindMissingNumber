ca8ae38835e20b3d08eecf8edfa7bbe28963db0b
Dragoş <edragos33@gmail.com>
2025-02-03 09:59:27 +0100

    Find 2 missing numbers

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     static int missingNumber(int[] arr) {
@@ -13,8 +16,38 @@ public class Main {
         return missingSum - sum;
     }
 
+    static List<Integer> missingMultipleNumber(int[] arr) {
+        // Find the highest number
+        int n = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > n) {
+                n = arr[i];
+            }
+        }
+
+        // Find existing numbers
+        boolean[] present = new boolean[n + 1];
+        for(int i = 0; i < arr.length; i++) {
+            present[arr[i]] = true;
+        }
+
+        // Find the missing numbers
+        List<Integer> missingNumbers = new ArrayList<>();
+        for (int i = 1; i < arr.length; i++) {
+            if (present[i] == false) {
+                missingNumbers.add(i);
+            }
+        }
+
+        return missingNumbers;
+    }
+
+
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,6,7,8,9};
         System.out.println(missingNumber(arr));
+
+        int[] arr2 = {1,3,4,5,7,8,9};
+        System.out.println(missingMultipleNumber(arr2));
     }
 }
